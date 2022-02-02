3ae97fce06f93bd0ba05a6741176cce3ee2f27d3
Marco Parra <marcoparra311@gmail.com>
2022-02-02 11:50:00 -0400

    Fix movies and heroImage issue when making mispells at search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,11 @@ import Spinner from './Spinner';
 const Home = () => {
   const { state, loading, error, searchRef, handleSearchValue, searchTerm, setIsLoadingMore } = useHomeFetch();
 
-  console.log('new state');
-  console.log(state);
-
   if (error) return <div> Plop...! </div>
 
   return (
     <>
-      {state.results[0] === undefined ? null : 
+      {state.results[0] !== undefined ? 
         (
           <>
             <HeroImage 
@@ -25,15 +22,16 @@ const Home = () => {
               text={state.results[0].overview} 
               title={state.results[0].original_title}
             />
-            <SearchBar searchRef={searchRef} handleSearchValue={handleSearchValue} />
-            <Grid header={searchTerm ? 'Seacrh Results' : 'Popular movies'} state={state} posterSize={POSTER_SIZE} imageUrl={IMAGE_BASE_URL} />
-            { loading && <Spinner /> }
-            { state.page < state.total_pages && !loading && (
-              <Button text='Load more' callback={() => setIsLoadingMore(true) } />
-            )}
           </>
-        )
-      }    
+        ) : null
+      }
+
+       <SearchBar searchRef={searchRef} handleSearchValue={handleSearchValue} />
+       <Grid header={searchTerm ? 'Seacrh Results' : 'Popular movies'} state={state} posterSize={POSTER_SIZE} imageUrl={IMAGE_BASE_URL} />
+       { loading && <Spinner /> }
+       { state.page < state.total_pages && !loading && (
+         <Button text='Load more' callback={() => setIsLoadingMore(true) } />
+       )}  
     </>
   )
 }
